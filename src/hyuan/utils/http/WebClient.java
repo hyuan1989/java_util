1cc3d3bd9b85d82f4fbf9f7b5cae6852b170c6e0
hyuan1989 <790868319@qq.com>
2016-08-04 15:41:56 +0800

    httpclient 多线程并发工具类 修改

--- src/hyuan/utils/http/WebClient.java
+++ src/hyuan/utils/http/WebClient.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.conn.scheme.PlainSocketFactory;
@@ -72,7 +73,7 @@ public class WebClient {
 	 */
 	public  static String httpRequestPost(String postData, String url)
 			throws Exception {
-		HttpPost httpost = HttpClientConnectionManager.getPostMethod(url);
+		HttpPost httpost = getPostMethod(url);
 		String jsonStr="";
 		HttpResponse response =null;
 		try{		
@@ -111,6 +112,39 @@ public class WebClient {
 	        if (hrb != null && hrb.isAborted()) {  
 	            hrb.abort();  
 	        }  
-	    }  
+	    }
+	    
+		/**
+		 * 模拟浏览器post提交
+		 * 
+		 * @param url
+		 * @return
+		 */
+		public static HttpPost getPostMethod(String url) {
+			HttpPost pmethod = new HttpPost(url); // 设置响应头信息
+			pmethod.addHeader("Connection", "keep-alive");
+			pmethod.addHeader("Accept", "*/*");
+			pmethod.addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+			pmethod.addHeader("Host", "mp.weixin.qq.com");
+			pmethod.addHeader("X-Requested-With", "XMLHttpRequest");
+			pmethod.addHeader("Cache-Control", "max-age=0");
+			pmethod.addHeader("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0) ");
+			return pmethod;
+		}
+
+		/**
+		 * 模拟浏览器GET提交
+		 * @param url
+		 * @return
+		 */
+		public static HttpGet getGetMethod(String url) {
+			HttpGet pmethod = new HttpGet(url);
+			// 设置响应头信息
+			pmethod.addHeader("Connection", "keep-alive");
+			pmethod.addHeader("Cache-Control", "max-age=0");
+			pmethod.addHeader("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0) ");
+			pmethod.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/;q=0.8");
+			return pmethod;
+		}
 	
 }
